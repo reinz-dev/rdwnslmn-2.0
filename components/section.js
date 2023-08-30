f51e42421af7981309d1470ba3deee8d7c23f4a4
reinz <reinz.developer@gmail.com>
2023-08-30 12:48:10 +0700

    modified section

--- components/section.js
+++ components/section.js
@@ -12,7 +12,6 @@ const Section = ({ children, delay = 0 }) => (
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.8, delay }}
-    alignItems="center"
     mb={6}
     mt={6}
   >
