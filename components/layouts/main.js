601e8027dcdbbc9278158055d0f38ada4d52d369
reinz <reinz.developer@gmail.com>
2023-08-30 14:31:07 +0700

    adding particles bg

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -3,10 +3,12 @@ import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
+import ParticlesBg from "../particlesBg";
 
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
+      <ParticlesBg />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="rdwnslmn homepage" />
