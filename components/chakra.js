3390edac14d17acd47404fb6c639bb785e477afd
reinz <110806427+reinzcity@users.noreply.github.com>
2023-08-30 21:10:54 -0700

    add preloader n set preloader

--- components/chakra.js
+++ components/chakra.js
@@ -3,17 +3,28 @@ import {
   cookieStorageManagerSSR,
   localStorageManager
 } from '@chakra-ui/react'
+import { useEffect, useState } from 'react';
 import theme from '../lib/theme'
+import Preloader from './preloader';
 
 export default function Chakra({ cookies, children }) {
   const colorModeManager =
     typeof cookies === 'string'
       ? cookieStorageManagerSSR(cookies)
       : localStorageManager
+      
+    const [loading, setLoading] = useState(true);
+
+      useEffect(() => {
+        // Simulasikan waktu loading dengan setTimeout
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000); // Ganti waktu sesuai kebutuhan Anda
+      }, []);
 
   return (
     <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
-      {children}
+      {loading ? <Preloader /> : children}
     </ChakraProvider>
   )
 }
